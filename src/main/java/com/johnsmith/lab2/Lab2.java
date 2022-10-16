6dd6ce57832eafa4f8d1aeffa4e3a6f339ec2eed
Eugene Ilyin <iea2001@mail.ru>
2022-10-16 23:28:04 +0700

    лабораторная работа №2

--- src/main/java/com/johnsmith/lab2/Lab2.java
+++ src/main/java/com/johnsmith/lab2/Lab2.java
@@ -5,22 +5,26 @@ import com.johnsmith.lab1.Lab1;
 import java.io.*;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Lab2 {
 
 
-    public static void shamirCipher(String nameFile) throws URISyntaxException {
-        byte[] bytes = readFile(nameFile);
+    public static void startCiphers(String fileName) throws URISyntaxException {
+        byte[] bytes = readFile(fileName);
+        String fileExtension = '.' + (String) Arrays.stream(fileName.split("\\.")).toArray()[1];
+        shamir(bytes, "./result/resultShamir" + fileExtension);
+        elGamal(bytes, "./result/resultElGamal" + fileExtension);
+        rsa(bytes, "./result/resultRSA.txt");
+        vernam(bytes, "./result/resultVernam" + fileExtension);
+
     }
 
-    private static byte[] readFile(String nameFile) throws URISyntaxException {
-        URL resource = Lab2.class.getResource('/' + nameFile);
+    private static byte[] readFile(String fileName) throws URISyntaxException {
+        URL resource = Lab2.class.getResource('/' + fileName);
         assert resource != null;
         File file = new File(resource.toURI());
-        List<String> list = new ArrayList<>();
+
         try (BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(file))) {
             return inputStream.readAllBytes();
         } catch (IOException e) {
@@ -28,11 +32,25 @@ public class Lab2 {
         }
 
         return new byte[]{};
+
     }
 
-    public static void shamir(long m) {
+    private static void writeFile(byte[] data, String fileName) throws URISyntaxException {
+        File file = new File(fileName);
+
+        try (BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(file))) {
+            outputStream.write(data);
+            outputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void shamir(byte[] data, String resultFileName) throws URISyntaxException {
         boolean flag = true;
         long p = 0;
+
         while (flag) {
             p = (long) (Math.random() * 1000000000) + 3;
             if (Lab1.testFerma(p, 1000)) {
@@ -40,75 +58,152 @@ public class Lab2 {
             }
         }
 
-        System.out.println("P = " + p);
-
-        long ca = (long) ((Math.random() * 10000) + 3);
+        long ca = (long) ((Math.random() * 1000000) + 3);
         while (Lab1.gcd(ca, p - 1) != 1) {
-            ca = (long) (Math.random() * 10000) + 3;
+            ca = (long) (Math.random() * 100) + 3;
         }
 
+        long da = Lab1.euclidAlgorithm(ca, p - 1)[1];
+        if (da < 0) {
+            da += p - 1;
+        }
 
-        long[] gcd = Lab1.euclidAlgorithm(ca, p - 1);
-        System.out.println(Arrays.toString(gcd));
-        long da = gcd[1] % (p - 1);
-
-        long cb = (long) (Math.random() * 10000) + 3;
+        long cb = (long) (Math.random() * 1000000) + 3;
         while (Lab1.gcd(cb, p - 1) != 1) {
-            cb = (long) (Math.random() * 10000) + 3;
+            cb = (long) (Math.random() * 1000000) + 3;
         }
 
-        gcd = Lab1.euclidAlgorithm(cb, p - 1);
-        long db = gcd[1] % (p - 1);
+        long db = Lab1.euclidAlgorithm(cb, p - 1)[1];
+        if (db < 0) {
+            db += p - 1;
+        }
 
+        byte[] decode = new byte[data.length];
+        for (int i = 0; i < data.length; i++) {
+            long x1 = Lab1.exponentiation(data[i], ca, p);
+            long x2 = Lab1.exponentiation(x1, cb, p);
+            long x3 = Lab1.exponentiation(x2, da, p);
+            long x4 = Lab1.exponentiation(x3, db, p);
+            decode[i] = (byte) x4;
+        }
 
-//        long ca = (long) (Math.random() * 1000000000) + 3;
-//        long da = (long) (Math.random() * 1000000000) + 3;
-//
-//        while ((ca * da) % (p - 1) != 1) {
-//            ca = (long) (Math.random() * 1000000000) + 3;
-//
-//            da = (long) (Math.random() * 1000000000) + 3;
-//
-//        }
-//
-//        long cb = (long) (Math.random() * 1000000000) + 3;
-//
-//        long db = (long) (Math.random() * 1000000000) + 3;
-//        while ((cb * db) % (p - 1) != 1) {
-//            cb = (long) (Math.random() * 1000000000) + 3;
-//            db = (long) (Math.random() * 1000000000) + 3;
-//        }
-//
-//        System.out.println(ca);
-//        System.out.println(da);
-//        System.out.println(cb);
-//        System.out.println(db);
-
-
-        long x1 = Lab1.exponentiation(m, ca, p);
-        long x2 = Lab1.exponentiation(x1, cb, p);
-        long x3 = Lab1.exponentiation(x2, da, p);
-        long x4 = Lab1.exponentiation(x3, db, p);
-        System.out.println("X1 = " + x1);
-        System.out.println("X2 = " + x2);
-        System.out.println("X3 = " + x3);
-        System.out.println("X4 = " + x4);
+        writeFile(decode, resultFileName);
 
     }
 
 
-    public static void elGamal(long m) {
+    public static void elGamal(byte[] data, String resultFileName) throws URISyntaxException {
         boolean flag = true;
         long p = 0;
         long q = 0;
         while (flag) {
-            p = (long) (Math.random() * 1000000000) + 3;
+            p = (long) (Math.random() * 1000) + 3;
             if (Lab1.testFerma(p, 1000)) {
                 flag = false;
             }
         }
 
 
+        long g = (long) ((Math.random() * p - 1) + 3);
+        while (Lab1.exponentiation(g, 1, p) != 1) {
+            g = (long) ((Math.random() * p - 1) + 3);
+        }
+
+
+        long x = (long) ((Math.random() * p - 1) + 1);
+        long k = (long) ((Math.random() * p - 2) + 2);
+        long y = Lab1.exponentiation(g, x, p);
+        long a = Lab1.exponentiation(g, k, p);
+
+        byte[] decode = new byte[data.length];
+        for (int i = 0; i < data.length; i++) {
+            long b = data[i] * Lab1.exponentiation(y, k, p);
+            long m1 = (b * Lab1.exponentiation(a, p - 1 - x, p)) % p;
+            decode[i] = (byte) m1;
+        }
+
+        writeFile(decode, resultFileName);
+
+    }
+
+    public static void rsa(byte[] data, String resultFileName) throws URISyntaxException {
+        boolean flag = true;
+        long p = 0;
+        long q = 0;
+
+        while (flag) {
+            p = (long) (Math.random() * 10000) + 3;
+            if (Lab1.testFerma(p, 100)) {
+                flag = false;
+            }
+        }
+
+        flag = true;
+        while (flag) {
+            q = (long) (Math.random() * 10000) + 3;
+            if (Lab1.testFerma(q, 100)) {
+                flag = false;
+            }
+        }
+
+        long n = p * q;
+
+        long f = (p - 1) * (q - 1);
+
+        long d = (long) ((Math.random() * (f - 1)) + 1);
+        while (Lab1.gcd(d, f) != 1) {
+            d = (long) ((Math.random() * (f - 1)) + 1);
+        }
+
+        long c = Lab1.euclidAlgorithm(d, f)[1];
+        if (c < 0) {
+            c += f;
+        }
+
+        byte[] decode = new byte[data.length];
+        for (int i = 0; i < data.length; i++) {
+            long e = Lab1.exponentiation(data[i], d, n);
+            long m1 = Lab1.exponentiation(e, c, n);
+            decode[i] = (byte) m1;
+        }
+
+        writeFile(decode, resultFileName);
+
+    }
+
+    public static void vernam(byte[] data, String resultFileName) throws URISyntaxException {
+        int currPositionDecode = 0;
+        byte[] result = new byte[data.length];
+
+        for (byte datum : data) {
+            String binaryMessage = Integer.toBinaryString(datum);
+            byte[] decode = new byte[binaryMessage.length()];
+            StringBuilder key = new StringBuilder();
+
+            for (int j = 0; j < binaryMessage.length(); j++) {
+                key.append((int) (Math.random() * 1));
+            }
+
+            StringBuilder xor = new StringBuilder();
+            for (int j = 0; j < binaryMessage.length(); j++) {
+                xor.append(binaryMessage.charAt(j) ^ key.charAt(j));
+            }
+
+            for (int j = 0; j < binaryMessage.length(); j++) {
+                decode[j] = (byte) (xor.charAt(j) ^ key.charAt(j));
+            }
+
+            long byteFromBinary = 0;
+            for (int j = 0; j < binaryMessage.length(); j++) {
+                if (decode[j] == 1) {
+                    byteFromBinary += Math.pow((double) 2, (double) binaryMessage.length() - 1 - j);
+                }
+            }
+            result[currPositionDecode] = (byte) byteFromBinary;
+            currPositionDecode++;
+        }
+
+        writeFile(result, resultFileName);
 
     }
 
