93248332c6ecc6308b412526256c44c57ed1f02b
Eugene Ilyin <iea2001@mail.ru>
2022-10-16 23:22:46 +0700

    создание отдельных методов для запуска первой и второй лабораторной работы

--- src/main/java/com/johnsmith/Main.java
+++ src/main/java/com/johnsmith/Main.java
@@ -7,35 +7,39 @@ import java.net.URISyntaxException;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-//        long aExp = 2;
-//        long xExp = 301;
-//        long pExp = 11;
-//        System.out.println("Fast mod exponentiation: ");
-//        System.out.println(aExp + "^" + xExp + " % " + pExp + " = " + Lab1.exponentiation(aExp, xExp, pExp));
-//
-//        long aEuclid = 28;
-//        long bEuclid = 19;
-//        System.out.println("\nEuclid algorithm:\n(" + aEuclid + "," + bEuclid + ") = " + Arrays.toString(Lab1.euclidAlgorithm(aEuclid, bEuclid)));
-//
-//        System.out.println("\nDiffie–Hellman:");
-//        Lab1.diffy();
-//
-//        long yBabyStep = 9;
-//        long pBabyStep = 23;
-//        long aBabyStep = 2;
-//
-//        System.out.println("\nBabyStepGiantStep:");
-//        System.out.println("y = " + yBabyStep + " a = " + aBabyStep + " p = " + pBabyStep + " => x = " + Lab1.babyStepGiantStep(yBabyStep, pBabyStep, aBabyStep));
+    public static void main(String[] args) throws URISyntaxException {
+        startLab2();
 
-            Lab2.shamir(100);
-
-//        try {
-//            Lab2.shamirCipher("text1.txt");
-//        } catch (URISyntaxException e) {
-//            e.printStackTrace();
-//        }
+    }
 
 
+    static void startLab1() {
+        long aExp = 2;
+        long xExp = 301;
+        long pExp = 11;
+        System.out.println("Fast mod exponentiation: ");
+        System.out.println(aExp + "^" + xExp + " % " + pExp + " = " + Lab1.exponentiation(aExp, xExp, pExp));
+
+        long aEuclid = 28;
+        long bEuclid = 19;
+        System.out.println("\nEuclid algorithm:\n(" + aEuclid + "," + bEuclid + ") = " + Arrays.toString(Lab1.euclidAlgorithm(aEuclid, bEuclid)));
+
+        System.out.println("\nDiffie–Hellman:");
+        Lab1.diffy();
+
+        long yBabyStep = 9;
+        long pBabyStep = 23;
+        long aBabyStep = 2;
+
+        System.out.println("\nBabyStepGiantStep:");
+        System.out.println("y = " + yBabyStep + " a = " + aBabyStep + " p = " + pBabyStep + " => x = " + Lab1.babyStepGiantStep(yBabyStep, pBabyStep, aBabyStep));
+        System.out.println(Lab1.exponentiation(7, -1, 22));
+
+    }
+
+
+    static void startLab2() throws URISyntaxException {
+        Lab2.startCiphers("test.txt");
+//        Lab2.startCiphers("lamb.png");
     }
 }
